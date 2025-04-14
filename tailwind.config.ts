482fe6d6bc4439909bfa5d5a36be6086a09c52e0
cinos81 <cinos81@gmail.com>
2025-04-14 09:49:45 +0900

    Tailwind CSS 설정 파일에서 문자열 따옴표를 변경하고, content 배열의 형식을 일관되게 수정했습니다.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "tailwindcss"
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  darkMode: "class",
+  darkMode: 'class',
   content: [
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "*.{js,ts,jsx,tsx,mdx}",
-  ],
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    '*.{js,ts,jsx,tsx,mdx}'
+  ]
 }
 
 export default config
