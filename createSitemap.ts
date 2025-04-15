1daa0fd3c959c4e7d91161f365c0705f1d9b50f9
cinos81 <cinos81@gmail.com>
2025-04-15 12:36:16 +0900

    package.json에 새로운 스크립트(postbuild) 추가 및 dotenv, sitemap, tsx 의존성 추가. pnpm-lock.yaml 업데이트. react-router.config.ts에서 prerender 설정 추가.

---
+++ createSitemap.ts
@@ -0,0 +1,32 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import { SitemapStream, streamToPromise } from 'sitemap'
+import { prerender } from './react-router.config'
+import 'dotenv/config'
+
+// eslint-disable-next-line @typescript-eslint/no-shadow
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+// 배포 디렉토리가 dist 일때 동작한다.
+const outputPath = path.join(__dirname, 'build/client', 'sitemap.xml')
+// 호스트 이름을 바꿔준다.
+const hostname = process.env.VITE_DOMAIN
+
+async function generateSitemap() {
+  const sitemapStream = new SitemapStream({ hostname })
+
+  prerender.forEach((route) => {
+    sitemapStream.write({
+      url: route,
+      changefreq: 'weekly',
+      priority: 0.8
+    })
+  })
+  sitemapStream.end()
+
+  const sitemap = await streamToPromise(sitemapStream)
+
+  fs.writeFileSync(outputPath, sitemap.toString())
+  console.log(`${outputPath} generated!`)
+}
+
+generateSitemap()
--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from '@react-router/dev/config'
 
+export const prerender = ['/']
+
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: false
+  ssr: false,
+  prerender
 } satisfies Config
